cf26723b009054df8840eaa881d617b8148a2191
Abel Monterroso <marvinabelmon@gmail.com>
2015-07-06 17:59:52 -0600

    Ultima version

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import application
 import mock
 import unittest
 
-class BorrarTestCase(unittest.TestCase):
+class GestorDeArchivosTest(unittest.TestCase):
 
     @mock.patch('application.os.path')
     @mock.patch('application.os')
@@ -20,13 +20,18 @@ class BorrarTestCase(unittest.TestCase):
         application.eliminar_archivo("archivo_existente")
         mock_os.remove.assert_called_with("archivo_existente")
 
-#Esta es la que no sivio
-#class CrearTestCase(unittest.TestCase):
+    @mock.patch('application.os.path')
+    @mock.patch('__builtin__.open')
+    def test_crear_archivo_inexistente(self, mock_open, mock_path):
+        mock_path.isfile.return_value = False
+        application.decidir_creacion("archivo_inexistente")
+        mock_open.assert_called_with("archivo_inexistente", "w")
 
-#    @mock.patch('application.os.path')
-#    @mock.patch('application.os')
-#    def test_crear_archivo(self, mock_os, mock_path):
-#        mock_path.isfile.return_value = False
-#        application.decidir_creacion('archivo_inexistente')
-#        self.assertTrue(mock_os.open.called, "Error! El archivo no deberia eliminarse porque no existe")
+    @mock.patch('application.os.path')
+    @mock.patch('__builtin__.open')
+    def test_crear_archivo_existente(self, mock_open, mock_path):
+        mock_path.isfile.return_value = True
+        application.decidir_creacion("archivo_existente")
+        self.assertFalse(mock_open.called, "Error! El archivo no debe crearse porque ya existe")
+
 
